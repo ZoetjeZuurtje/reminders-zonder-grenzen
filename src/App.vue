9e868c8147ee316b6f0bbfafd86bc8c270893acc
Bram <bramstrik@live.com>
2026-05-11 19:24:33 +0200

    Add countdown timer

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import Countdown from './components/Countdown.vue'
+const targetDate = new Date('2026-05-13T18:00:00Z');
+</script>
+
+<template>
+  <div>
+    <Header />
+
+    <main id="center">
+      <div>
+        <h2>Je gaat die boekenlegger meenemen.</h2>
+        <p class="lead">Ik heb alle vertrouwen in je</p>
+      </div>
+
+      <Countdown :target='targetDate'/>
+    </main>
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+        <script setup lang="ts">
+        </script>
+
+<template>
+	<header id="site-header">
+        <!-- <img src='./../../public/kraken.jpg' alt="Kraken" width="100"> -->
+		<div class="container">
+			<h1>Reminders zonder Grenzen</h1>
+			<p class="tagline">We weten je te vinden</p>
+		</div>
+	</header>
+</template>
+
+
+<style scoped>
+#site-header {
+	padding: 20px 0;
+	border-bottom: 1px solid var(--border);
+    .container {
+        text-align: left;
+    }
+}
+.tagline {
+	margin-top: 6px;
+	color: var(--text);
+}
+</style>
