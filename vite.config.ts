cb9a6c88ad1b351ceb9b1be909fcc87658bd6932
Bram <bramstrik@live.com>
2026-05-12 06:47:38 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/reminders-zonder-grenzen/',
+  base: '/reminders-zonder-grenzen/dist/',
   plugins: [vue()],
 })
